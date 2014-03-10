5ed99879820e70743b239cc553621d7a73ae4bdf
Guy Ellis <wildfiction@gmail.com>
2014-03-09 20:40:39 -0700

    Improved mongoose connection logging.

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 // development only
@@ -58,9 +58,10 @@ app.get('/tree', function(req,res){
 })
 
 app.post('/tree', function(req,res){
-    data.create(req.body);
-    data.read(function(err, trees) {
-        res.render('tree', {trees: trees});
+    data.create(req.body, function(err) {
+        data.read(function(err, trees) {
+            res.render('tree', {trees: trees});
+        });
     });
 })
 
--- models/dataman.js
+++ models/dataman.js
@@ -1,4 +1,35 @@
-var mongoose = require('mongoose');
+// Bring Mongoose into the app
+var mongoose = require( 'mongoose' );
+
+// Build the connection string
+var dbURI = 'mongodb://localhost/test';
+
+// Create the database connection
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('open', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
 
 var orchardSchema = mongoose.Schema({
     tree: String,
@@ -7,30 +38,22 @@ var orchardSchema = mongoose.Schema({
 
 var Orchard = mongoose.model('Tree', orchardSchema);
 
-exports.create = function(tree) {
-    mongoose.connect('mongodb://localhost/test');
-
+exports.create = function(tree,cb) {
     var orchard = new Orchard(tree);
     orchard.save(function (err) {
         if (err) {
             console.log('error saving tree');
         }
-        mongoose.connection.close();
+        cb(err);
     });
 }
 
 exports.read = function(cb) {
-    mongoose.connect('mongodb://localhost/test');
-
-    // var orchard = new Orchard();
-    //console.log('dataman.js read');
     Orchard.find(function (err, trees) {
-        //console.log('read find');
         if (err) {
             console.log('error getting trees');
         }
-        // console.dir(trees);
-        mongoose.connection.close();
+        // mongoose.connection.close();
         cb(err, trees);
     });
 }
