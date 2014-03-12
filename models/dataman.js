445a59a645e77865db5692f992143e6042e2499b
Guy Ellis <gellis@godaddy.com>
2014-03-12 09:33:26 -0700

    Moved event code to bottom of file.

--- models/dataman.js
+++ models/dataman.js
@@ -7,30 +7,6 @@ var dbURI = 'mongodb://localhost/test';
 // Create the database connection
 mongoose.connect(dbURI);
 
-// CONNECTION EVENTS
-// When successfully connected
-mongoose.connection.on('open', function () {
-    console.log('Mongoose default connection open to ' + dbURI);
-});
-
-// If the connection throws an error
-mongoose.connection.on('error',function (err) {
-    console.log('Mongoose default connection error: ' + err);
-});
-
-// When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-    console.log('Mongoose default connection disconnected');
-});
-
-// If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
-});
-
 var orchardSchema = mongoose.Schema({
     tree: String,
     quantity: Number
@@ -57,3 +33,40 @@ exports.read = function(cb) {
         cb(err, trees);
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('open', function () {
+    console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
