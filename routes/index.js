0a63f061049e28c50316943441e1d260fd431cb1
Guy Ellis <gellis@godaddy.com>
2014-04-04 22:40:04 -0700

    Fixed title

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Fluent 2014 - Introduction to ExpressJS' });
+  res.render('index', { title: 'ExpressJS and MongoDB' });
 };
